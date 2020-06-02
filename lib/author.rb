94e624e9f6251fd875c655ff441996d147e87a92
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-02 04:27:35 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,20 @@
 class Author
-  attr_accessor :name, :post
+  attr_accessor :name, :author
+  
+  def initialize(name)
+    @posts =[]
+    @name = name
+  end
+  
+   def posts
+    Post.all.select {|x| x.author == self}
+  end
+  
+  def add_post(post)
+   post.author = self
+  end
+  
+    def add_post_by_title(post_name)
+    self.add_post(Post.new(post_name))
+  end
 end
--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,12 @@
 class Post
   attr_accessor :author, :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 end
